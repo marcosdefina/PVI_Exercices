5955f3386e02c00b22602f1c5835eb893a265004
Marcos Defina <marcosdefina@gmail.com>
2018-12-04 21:28:41 -0200

    restoring essential deleted files, the site get back working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,13 +24,6 @@ import { InMemoryDataService }  from './in-memory-data.service';
     HeroDetailComponent,
     MessagesComponent,
     DashboardComponent,
-    
-    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
-    // and returns simulated server responses.
-    // Remove it when a real server is ready to receive requests.
-      HttpClientInMemoryWebApiModule.forRoot(
-        InMemoryDataService, { dataEncapsulation: false }
-      )
   ],
   imports: [
     HttpClientInMemoryWebApiModule,
