bbc8e20a59d962b173c49f5b573aff42894c76be
Marcos Defina <marcosdefina@gmail.com>
2018-12-04 20:44:43 -0200

    Saving Progress. Not working yet!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,10 @@ import { FormsModule } from '@angular/forms';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { DashboardComponent } from './dashboard/dashboard.component'; // <-- NgModel lives here
+import { HttpClientModule }   from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
 
 @NgModule({
   declarations: [
@@ -20,13 +24,24 @@ import { DashboardComponent } from './dashboard/dashboard.component'; // <-- NgM
     HeroDetailComponent,
     MessagesComponent,
     DashboardComponent,
+    
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // and returns simulated server responses.
+    // Remove it when a real server is ready to receive requests.
+      HttpClientInMemoryWebApiModule.forRoot(
+        InMemoryDataService, { dataEncapsulation: false }
+      )
   ],
   imports: [
+    HttpClientInMemoryWebApiModule,
+    HttpClientModule,
     BrowserModule,
     FormsModule,
     AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
+
+  
 })
 export class AppModule { }
